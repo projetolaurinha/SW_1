8cf5b35cadb42f6bf9721879507d4ae7fd75be35
Laura de Oliveira Sousa <100208612+projetolaurinha@users.noreply.github.com>
2023-03-14 12:17:43 -0300

    Add files via upload

---
+++ alunos/ExercicioAlunos/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ExercicioAlunos;
+class Program
+{
+    static void Main(string[] args)
+    {
+       Alunos aluno1 = new Alunos();
+       aluno1.nome = "Luvas Prates";
+       aluno1.nota1 = 4.3;
+       aluno1.nota2 = 7.8;
+
+       aluno1.mensagem();
+    }
+}
