6069386c1c93a90dd24f796dd62f0d0d9589a55b
Laura de Oliveira Sousa <100208612+projetolaurinha@users.noreply.github.com>
2023-03-28 12:31:50 -0300

    Add files via upload

---
+++ loja/proj_loja/Produto.cs
@@ -0,0 +1,35 @@
+﻿namespace proj_loja;
+class Produto
+{
+
+    public Produto (string nome, int desconto){
+        this.Nome= nome;
+        this.Preco = 10 -(10*desconto/100);
+    }
+   public Produto (string nome){
+        this.Nome= nome;
+        this.Preco = 10;
+    }
+
+     public Produto (){
+        this.Nome= "TicToc";
+        this.Preco = 10;
+    }
+
+   private string? Nome {get;set;}
+
+   private double Preco {get;set;}
+
+   public void Alteranome (string nome){
+    this.Nome = nome;
+   }
+
+   public void Alterapreco (double preco){
+    this.Preco = preco;
+   }
+
+   public void MostraDados(){
+    Console.WriteLine("Nome: "+ this.Nome);
+    Console.WriteLine("Preco: " + this.Preco);
+   }
+}
---
+++ loja/proj_loja/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace proj_loja;
+class Program
+{
+    static void Main(string[] args)
+    {
+       // Produto p1 = new Produto();
+      // Produto p1 = new Produto("XYZ");
+      Produto p1 = new Produto ("ABC", 10);
+      p1.MostraDados();
+    }
+}
