5ebc79be18bba0b7ae1242dea2c26b9925d4bebb
Laura de Oliveira Sousa <100208612+projetolaurinha@users.noreply.github.com>
2023-03-28 12:01:12 -0300

    Add files via upload

---
+++ proj_pessoa/Pessoa.cs
@@ -0,0 +1,42 @@
+﻿namespace proj_pessoa;
+class Pessoa
+
+{
+    public Pessoa(){
+        this.Nome = "Não informado";
+        this.Sobrenome = "Não informado";
+        this.Idade = 0;
+
+    }
+
+    public Pessoa(string nome, int idade){
+        this.Nome = nome;
+        this.Sobrenome = "não informado";
+        this.Idade= idade;
+    }
+      private string? Nome {get;set;}
+
+      private string? Sobrenome {get;set;}
+ 
+      private int Idade {get;set;}
+
+      public void Alteranome(string nome){
+        this.Nome= nome;
+      }
+
+      public void AlteraSobrenome (string sobrenome){
+        this.Sobrenome = sobrenome;
+      }
+
+      public void Alteraidade (int idade){
+        this.Idade = Idade;
+      }
+
+      public void MostraDados (){
+        Console.WriteLine ("Nome: " + this.Nome);
+        Console.WriteLine ("Sobrenome: " + this.Sobrenome);
+        Console.WriteLine ("Idade: " + this.Idade);
+      }
+
+
+}
---
+++ proj_pessoa/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace proj_pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa ("godofredo",18);
+        //p1.Alteranome("Laura");
+        //p1.AlteraSobrenome("Oliveira");
+        //p1.Alteraidade(16);
+        p1.MostraDados();
+    }
+}
